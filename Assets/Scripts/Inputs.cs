a60bc9b12f6b36d9cda53bf5b185251406404926
Unknown <svenjanssen12321@gmail.com>
2020-07-11 15:27:54 +0200

    Controller lets player rotate

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour
+{
+    [Header("Movement keys")]
+    public KeyCode Up;
+    public KeyCode Left;
+    public KeyCode Down;
+    public KeyCode Right;
+
+    public static Inputs Instance;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+}
---
+++ Assets/Scripts/cameramover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameramover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * Time.fixedDeltaTime);
+    }
+}
